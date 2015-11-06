a7b89c873e0aba65fb7c206c65f21dce982b297d
Cyrois <cyrois3@gmail.com>
2015-11-05 16:55:15 -0800

    queue class

--- src/main.py
+++ src/main.py
@@ -1,13 +1,5 @@
-#!/usr/bin/python
-from buffer import Buffer
+import Queue
 
-
-#Test
-if __name__ == '__main__':
-    buf1 = Buffer("Nest 3rd Floor",1,2,3,4)
-    print buf1.location
-    print Buffer.__doc__
-    print buf1.getCount("blue")
-    for i in range(0,10):
-        buf1.incrementCount("blue")
-    print buf1.getCount("blue")
+class Main:
+    test = Queue(10)
+    test.push();
