26900d3c0785567fc363b30871455bd8d2c552d6
Calvin Chan <cyrois3@gmail.com>
2015-11-05 20:39:42 -0800

    Update Buffer.py

--- src/Buffer.py
+++ src/Buffer.py
@@ -60,27 +60,6 @@ class Buffer:
     
     
 #   def flushTable():
-
-<<<<<<< HEAD
 #   def initQueue():
 #   def roundRobinCheck():
-
-=======
-
 #   def pullTable():
-
-
-"""
->>>>>>> 666938c... Testing importing on main
-if __name__ == '__main__':
-    buf1 = Buffer("Nest 3rd Floor",1,2,3,4)
-    print buf1.location
-    print Buffer.__doc__
-    print buf1.getCount("blue")
-    for i in range(0,10):
-         buf1.incrementCount("blue")
-    print buf1.getCount("blue")
-"""
-
-
-
