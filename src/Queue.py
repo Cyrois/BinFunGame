662ccbc960ba4f3ceba1b754fc17a8359b4a7176
Cyrois <cyrois3@gmail.com>
2015-11-10 15:24:27 -0800

    done queue class, there is an error

--- src/Queue.py
+++ src/Queue.py
@@ -2,29 +2,44 @@ from collections import deque
 
 class Queue:
     __queue = deque()
-    test = deque(["a", "b", "c"])
+    __isEmpty = None
     
     def __init__(self):
-        print "init"
+        print "initializing queue"
                  
     def push(self, Signal):
         print "pushing: " + Signal
         self.__queue.append(Signal)
+        self.setEmptyFlag(False)
                  
     def flushQueue(self):
         print "flush queue"
-        #print test.queue
-        #list(test)
-        #for signal in test.queue:
-         #   print signal
-        #for signal in iter(self.get, self.test):
-         #   print test.pop
-        for signal in self.__queue:
+        queue = self.__queue.copy()
+        self.__queue.clear()
+        self.setEmptyFlag(True)
+        return queue
+        
+    def printQueue(queue):
+        for signal in queue:
             print signal
+
+    def setEmptyFlag(self, empty):
+        self.__isEmpty = empty
+        
+    def getEmptyFlag(self):
+        return self.__isEmpty
+        
 if __name__ == '__main__':
     print "testing"
     test = Queue()
     test.push("1")
     test.push("2")
     test.push("3")
-    test.flushQueue()
+    Queue.printQueue(test)
+    
+    newTest = test.flushQueue()
+    Queue.printQueue(newTest)
+    
+    print("something")
+    wait = input("PRESS ENTER TO CONTINUE.")
+    print("something")
