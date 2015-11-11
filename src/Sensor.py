851f1429f8a3fae856d5235867bb61c7bacd8b7a
Cyrois <cyrois3@gmail.com>
2015-11-10 16:04:36 -0800

    done sensor class

--- src/Sensor.py
+++ src/Sensor.py
@@ -1,33 +1,47 @@
 import RPi.GPIO as GPIO
 import datetime
+import Queue
 
 class Sensor:
-	"""
+    __isEmpty = None
+    __signal = ""
+    __ID = ""
+    __Location = ""
+        
+    #GPIO Input Pin for the Sensor
+    __gpiopin = -1
 
-	"""
+    #set up GPIO
+    def __init__(self, int, ID, Location):
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setwarnings(False)
+        self.gpiopin = int
+        GPIO.setup(self.gpiopin, GPIO.IN)
+        self.__ID = ID
+        self.__Location = Location
 
-	#GPIO Input Pin for the Sensor
-	__gpiopin = -1
+    #wait for sensor to turn on
+    def listenSignal(self):
+        while True:
+            GPIO.wait_for_edge(self.gpiopin, GPIO.RISING)
+            self.sendSignal()
 
-	#set up GPIO
-	def __init__(self, int):
-		GPIO.setmode(GPIO.BOARD)
-		GPIO.setwarnings(False)
-		self.gpiopin = int
-		GPIO.setup(self.gpiopin, GPIO.IN)
+    #refactor into signal class to parse and initialize signal
+    def sendSignal(self):           
+        #send date, time and id
+        #need to send to queue instead 
+        print datetime.datetime.utcnow()
+        self.__signal = self.initializeSignal()
+        self.setEmptyFlag(False)
 
-	#wait for sensor to turn on
-	def listenSignal(self):
-		while True:
-			GPIO.wait_for_edge(self.gpiopin, GPIO.RISING)
-			self.sendSignal()
-
-	def sendSignal(self):			
-		#send date, time and id
-		#need to send to queue instead 
-		print datetime.datetime.utcnow()
-		
-
+    def initializeSignal(self):
+        return self.__ID + "," + self.__Location + "," + datetime.datetime.utcnow()
+        
+    def setEmptyFlag(self, empty):
+        self.__isEmpty = empty
+        
+    def getEmptyFlag(self):
+        return self.__isEmpty
 
 
 
