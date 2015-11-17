ad1bdb4447ec3d5d1ac4fa6fbd221917bf396891
sebastian-lee <sebastian.lee.9@gmail.com>
2015-11-17 15:49:45 -0800

    Refactored Display

--- src/display/Display.py
+++ src/display/Display.py
@@ -4,59 +4,58 @@
 # import Buffer
 import web
 # from time import strftime
-
 count = 0
-blackCount = 0 # Buffer.blackCount
-greenCount = 0 # Buffer.greenCount
-blueCount = 0 # Buffer.blueCount
-greyCount = 0 # Buffer.greyCount
 
-# echo function
-def make_text(string):
-    global count
-    if string == "reset": #change to reset when time is 00:00 (midnight)
-        count = 0
-        return str(count)
-        #zeroString = "Count: " + str(count) + " - Message: '" + string + "' - Time: " + strftime("%Y-%m-%d %H:%M:%S")
-        #return zeroString
-    elif string != "reset":
-        count += 1
-        return str(count)
-        #countString = "Count: " + str(count) + " - Message: '" + string + "' - Time: " + strftime("%Y-%m-%d %H:%M:%S")
-        #return countString
+class Display:
 
-# says map the root URL address to what happens in the
-# tutorial class
-urls = ('/', 'tutorial')
-# if using templates, tell web.py where to find our
-# HTML templates with render
-render = web.template.render('templates/')
+    __blackCount = 0 # Buffer.blackCount
+    __greenCount = 0 # Buffer.greenCount
+    __blueCount = 0 # Buffer.blueCount
+    __greyCount = 0 # Buffer.greyCount
+    app = None
+    my_form = None
+    render = None
 
-app = web.application(urls, globals())
+    def __init__(self):
+        # says map the root URL address to what happens in the
+        # tutorial class
+        urls = ('/', 'Display')
+        # if using templates, tell web.py where to find our
+        # HTML templates with render
+        self.render = web.template.render('templates/')
+        self.app = web.application(urls, globals())
 
-# creates an HTML text entry box that we render
-my_form = web.form.Form(
-                        web.form.Textbox('', class_='textfield', id='textfield'),
-                        )
+        # creates an HTML text entry box that we render
+        self.my_form = web.form.Form(
+                                web.form.Textbox('', class_='textfield', id='textfield'),
+                                )
+            
+    # echo function
+    def make_text(self, string):
+        global count
+        if string == "reset": #change to reset when time is 00:00 (midnight)
+            count = 0
+            return str(count)
+            #zeroString = "Count: " + str(count) + " - Message: '" + string + "' - Time: " + strftime("%Y-%m-%d %H:%M:%S")
+            #return zeroString
+        elif string != "reset":
+            count += 1
+            print count
+            return str(count)
+            #countString = "Count: " + str(count) + " - Message: '" + string + "' - Time: " + strftime("%Y-%m-%d %H:%M:%S")
+            #return countString
 
-# define tutorial class, GET and POST methods define
-# the behaviour of our web.py app
-class tutorial:
     def GET(self):
         # give copy of the form instance
-        form = my_form()
+        form = self.my_form()
         # using same name tutorial in class def, call,
         # and HTML template are important
-        return render.tutorial(form, "0")
+        return self.render.Display(form, "0")
         
     def POST(self):
-        form = my_form()
+        form = self.my_form()
         form.validates()
         # access the value of our text field box
         s = form.value['textfield']
         # send to our echo function
-        return make_text(s)
-
-if __name__ == '__main__':
-    app.run()
-
+        return self.make_text(s)
---
+++ src/display/Html.py
@@ -0,0 +1,10 @@
+from Display import Display
+import web
+
+#count = 0
+
+if __name__ == '__main__':
+
+    Dis = Display();
+    Dis.make_text("update");
+    Dis.app.run()
