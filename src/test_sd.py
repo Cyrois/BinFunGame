1202e42a0211ecc70b52d71c1e0dc9d33fcfe2b4
Calvin Chan <cyrois3@gmail.com>
2015-12-03 11:11:22 -0800

    added test cases for sd

--- src/test_sd.py
+++ src/test_sd.py
@@ -2,10 +2,21 @@ from SDfile import SDfile
 import os
 import time
 
-def test_isCurrentDateTest(tmpdir):
+def test_isCurrentDate(tmpdir):
 	p = tmpdir.mkdir("sub")
-	print p
 	sd = SDfile(str(p), "location")
-	curTime = time.strftime("%d_%m_%Y")
-	print sd.isCurrentDate(curTime)
-	assert sd.isCurrentDate(curTime) == True
+	curDate = time.strftime("%d_%m_%Y")
+	assert sd.isCurrentDate(curDate) == True
+
+def test_getCurrentDate(tmpdir):
+	p = tmpdir.mkdir("sub")
+	sd = SDfile(str(p), "location")
+	curDate = time.strftime("%d_%m_%Y")
+	assert sd.getCurrentDate() == curDate
+
+def test_setCurrentDate(tmpdir):
+	p = tmpdir.mkdir("sub")
+	sd = SDfile(str(p), "location")
+	curDate = time.strftime("12_12_12")
+	sd.setCurrentDate(curDate)
+	assert sd.getCurrentDate() == curDate
