2e4a7748085bb790229c1d6704d342296a1e8de0
Calvin Chan <cyrois3@gmail.com>
2016-01-07 16:48:04 -0800

    updated ultra.py

---
+++ src/UltraSonic/Sensor_copy.py
@@ -0,0 +1,99 @@
+import RPi.GPIO as GPIO
+import datetime
+import Queue
+import time
+from Signal import Signal
+
+class Sensor:
+    __isEmpty = True    #flag used by Queue to see if the Sensor is active
+    __signal = ''   #the current Signal string, see Signal class for format
+    __ID = ''   #the sensor ID
+    __Location = '' #the location of the sensor
+        
+    #GPIO Input Pin for the Sensor
+    __gpiopin = 7
+
+    #set up GPIO
+    def __init__(self, ID, Location):
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setwarnings(False)
+        self.__gpiopin = self.getGPIOPin(ID)
+        GPIO.setup(self.__gpiopin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+        self.__ID = ID
+        self.__Location = Location
+	#time.sleep(10)
+        GPIO.add_event_detect(self.__gpiopin, GPIO.RISING, callback=self.testSendSignal)
+
+    def testSendSignal(self, pinNumber):
+        print "signal callback from signal: " + str(pinNumber)
+        self.sendSignal()
+
+    def getGPIOPin(self, ID):
+        if ID is "black":
+            return 7
+        elif ID is "green":
+            return 12
+        elif ID is "blue":
+            return 16
+        elif ID is "grey":
+            return 15
+        else:
+            return 0
+
+    #wait for sensor to turn on
+    def listenSignal(self):
+        #if(GPIO.input(self.__gpiopin)):
+        #    self.sendSignal()
+        #print "WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW"
+        #while True:
+        #GPIO.wait_for_edge(self.__gpiopin, GPIO.RISING)
+        #self.sendSignal()
+        return
+        
+    #refactor into signal class to parse and initialize signal
+    def sendSignal(self):           
+        #send date, time and id
+        #need to send to queue instead 
+        dateTime = datetime.datetime.utcnow()
+        print dateTime
+        self.__signal = Signal.initializeSignal(self.__ID, self.__Location, dateTime)
+        self.setEmptyFlag(False)
+
+    #sets the empty flag to the given empty variable, used by Queue class
+    def setEmptyFlag(self, empty):
+        self.__isEmpty = empty
+        
+    #returns the empty flag, used by Queue class    
+    def isEmpty(self):
+        return self.__isEmpty
+
+    def getSignal(self):
+        self.setEmptyFlag(True)
+        return self.__signal
+	
+        
+    # now we'll define two threaded callback functions  
+    # these will run in another thread when our events are detected  
+    def my_callback(channel):
+        print "falling edge detected on 15"
+    def my_callback2(channel):
+        print "falling edge detected on 12"
+    def my_callback3(channel):
+        print "falling edge detected on 18"
+    def my_callback4(channel):
+        print "falling edge detected on 7" 
+    	
+    # when a falling edge is detected on port __gpiopin, regardless of whatever   
+    # else is happening in the program, the function my_callback will be run
+    #__init__(self, 1, "Nest")
+    #GPIO.add_event_detect(19, GPIO.FALLING, callback=my_callback, bouncetime=300)
+    #GPIO.add_event_detect(12, GPIO.FALLING, callback=my_callback, bouncetime=300)
+    #GPIO.add_event_detect(18, GPIO.FALLING, callback=my_callback, bouncetime=300)
+    #GPIO.add_event_detect(7, GPIO.FALLING, callback=my_callback, bouncetime=300)
+	
+    #Another example of interrupts:
+    #try:  
+    #   GPIO.wait_for_edge(23, GPIO.FALLING)  
+    #except KeyboardInterrupt:  
+    #   GPIO.cleanup()       # clean up GPIO on CTRL+C exit  
+    #GPIO.cleanup()           # clean up GPIO on normal exit 
--- src/UltraSonic/ultra.py
+++ src/UltraSonic/ultra.py
@@ -1,53 +1,134 @@
 import RPi.GPIO as GPIO
 import time
-GPIO.setmode(GPIO.BCM)
+import datetime
+import Queue
+import thread
+from Signal import Signal
 
-TRIG = 23
-ECHO = 24
+class Sensor:
+	__isEmpty = True    #flag used by Queue to see if the Sensor is active
+    __signal = ''   #the current Signal string, see Signal class for format
+    __ID = ''   #the sensor ID
+    __Location = '' #the location of the sensor
+        
+    #GPIO Input Pin for the Sensor
+    __gpiopin = 7
 
-print "Distance Measurement in Progress"
+	__trig = ''
+	__echo = ''
+	__base_distance = ''
 
-GPIO.setup(TRIG, GPIO.OUT)
-GPIO.setup(ECHO, GPIO.IN)
+    #set up GPIO
+    def __init__(self, ID, Location):
+		self.__ID = ID
+        self.__Location = Location
 
-GPIO.output(TRIG, False)
-print "Waiting for sensor to settle"
-time.sleep(2)
+		GPIO.setmode(GPIO.BCM)
+		setGPIOPin(self)
 
-GPIO.output(TRIG, True)
-time.sleep(0.00001)
-GPIO.output(TRIG, False)
+		GPIO.setup(self.__trig, GPIO.OUT)
+		GPIO.setup(self.__echo, GPIO.IN)
+		calibrateSensor(self)
 
-while GPIO.input(ECHO) == 0:
-    pulse_start = time.time()
+		try:
+		   thread.start_new_thread( run, (self,) )
+		except:
+		   print "Error: unable to start thread"
 
-while GPIO.input(ECHO) == 1:
-    pulse_end = time.time()
+	def run(self):
+		while True:
+			GPIO.output(self.__trig, True)
+			time.sleep(0.00001)
+			GPIO.output(self.__trig, False)
 
-pulse_duration = pulse_end - pulse_start
-base_distance = pulse_duration * 17150
-base_distance = round(base_distance, 2)
+			while GPIO.input(self.__echo) == 0:
+				pulse_start = time.time()
 
-print "base distance is: ",base_distance, " cm"
+			while GPIO.input(self.__echo) == 1:
+				pulse_end = time.time()
+			
+			distance = calculateDistance(pulse_start, pulse_end)
 
-while True:
-    GPIO.output(TRIG, True)
-    time.sleep(0.00001)
-    GPIO.output(TRIG, False)
+			if distance < base_distance:
+			print "object detected at ", distance
 
-    while GPIO.input(ECHO) == 0:
-        pulse_start = time.time()
+			time.sleep(0.05)
+		
+	def calculateDistance(pulse_start, pulse_end):
+		pulse_duration = pulse_end - pulse_start
+		distance = pulse_duration * 17150
+		distance = round(distance, 2)
+		return distance
+	
+    def testSendSignal(self, pinNumber):
+        print "signal callback from signal: " + str(pinNumber)
+        self.sendSignal()
 
-    while GPIO.input(ECHO) == 1:
-        pulse_end = time.time()
+    def setGPIOPin(self):
+        if self.__ID is "black":
+			self.__trig = 7
+			self.__echo = 11           
+			return
+        elif self.__ID is "green":
+			self.__trig = 13
+			self.__echo = 15       
+			return
+        elif self.__ID is "blue":
+			self.__trig = 18
+			self.__echo = 23           
+			return
+        elif self.__ID is "grey":
+			self.__trig = 24
+			self.__echo = 25       
+			return
+        else:
+            return 0
 
-    pulse_duration = pulse_end - pulse_start
-    distance = pulse_duration * 17150
-    distance = round(distance, 2)
+    def sendSignal(self):           
+        #send date, time and id
+        #need to send to queue instead 
+        dateTime = datetime.datetime.utcnow()
+        print dateTime
+        self.__signal = Signal.initializeSignal(self.__ID, self.__Location, dateTime)
+        self.setEmptyFlag(False)
 
-    if distance < base_distance:
-        print "object detected at ", distance
+    #sets the empty flag to the given empty variable, used by Queue class
+    def setEmptyFlag(self, empty):
+        self.__isEmpty = empty
+        
+    #returns the empty flag, used by Queue class    
+    def isEmpty(self):
+        return self.__isEmpty
 
-    time.sleep(0.05)
+    def getSignal(self):
+        self.setEmptyFlag(True)
+        return self.__signal
 
-GPIO.cleanup()
+	def calibrateSensor(self):
+		print "Calibrating Sensor ", self.__ID
+		print "Warming up..."
+		GPIO.output(self.__trig, False)
+		time.sleep(2)
+
+		#activate the sensor with 10ms high signal
+		GPIO.output(self.__trig, True) 
+		time.sleep(0.00001)
+		GPIO.output(self.__trig, False)
+		
+		#start calculating time of signal
+		while GPIO.input(self.__echo) == 0:
+			pulse_start = time.time()
+
+		while GPIO.input(self.__echo) == 1:
+			pulse_end = time.time()
+
+		self.__base_distance = distance = calculateDistance(pulse_start, pulse_end)
+		self.__base_distance = round(self.__base_distance)
+
+		print "base distance is: ", self.__base_distance, " cm"
+		print "Calibration Complete..."
+		return	
+
+test = Sensor("black", "Nest")	
+
+
