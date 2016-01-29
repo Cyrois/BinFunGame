a84efc44028bdd6f6c91a19d03ba97933c1461e4
s-teph <stephanieashleylam@hotmail.com>
2016-01-28 16:37:24 -0800

    Complete scoreboard database
    Create table, insert scores and get top scores.

--- src/Database.py
+++ src/Database.py
@@ -8,6 +8,7 @@ class Database:
     cursor = None
     currentDate = ""
     query = ""
+    sbquery = ""
       
     def __init__(self, theHost, theUser, thePsswd, theDB):
         self.db = MySQLdb.connect(theHost, theUser, thePsswd, theDB)
@@ -29,15 +30,15 @@ class Database:
 	def createCountTable(self, date):
 		print "Creating Count Table.."
         columns = ["Black","Green","Blue","Grey"]
-		query = "CREATE TABLE " + date + "_Count" + "(" + columns[0] + " INT," + columns[1] + " INT," + columns[2] + " INT," + columns[3] + " INT" + ");"
-		try: self.cursor.execute(query)
+        query = "CREATE TABLE " + date + "_Count" + "(" + columns[0] + " INT," + columns[1] + " INT," + columns[2] + " INT," + columns[3] + " INT" + ");"
+        try: self.cursor.execute(query)
         except MySQLdb.Error, e:
                 print "MySQL Error: " + str(e)
         else:
                 print("Table Creation Success")
 			
 	def insertCount(self, black, green, blue, grey):
-		query = "UPDATE " + date + "_count" + " SET Black =" + black + ",Green =" green + ",Blue =" + blue + ",Grey = " + grey + ";" #WHERE some_column=some_value;
+		query = "UPDATE " + date + "_count" + " SET Black =" + black + ",Green =" + green + ",Blue =" + blue + ",Grey = " + grey + ";" #WHERE some_column=some_value;
 		self.cursor.execute(query)
 		self.db.commit()
 	
@@ -75,21 +76,86 @@ class Database:
 		print "INSERTING INTO DATABASE"
         date = time.strftime("%d_%m_%Y") #get current date
         if not self.isCurrentDate(date): #check if the date has changed
-			self.createCountTable(date)
+            self.createCountTable(date)
             self.createBFGTable(date)
             self.setCurrentDate(date)
 			
-		self.insertCount(black, green, blue, grey)
-		self.insertBuffer(target)
+        self.insertCount(black, green, blue, grey)
+        self.insertBuffer(target)
 		
 	#Select: http://zetcode.com/db/mysqlpython/
 	#there should be only one row in the table
 	def pullCount(self):
 		query = "SELECT *" + " FROM " + self.currentDate + "_count;"
 		self.cursor.execute(query)
-		if( cursor.rowcount > 0 )
-			rows = cursor.fetchall()
+		if( self.cursor.rowcount > 0 ):
+			rows = self.cursor.fetchall()
 			result = rows[0]
 			return result
-		else
+		else:
 			print "ERROR: No rows"
+
+
+    #database functions for the sorting game application
+    #generates table for scores
+    def createScoreboardTable(self):
+        print "Creating Scoreboard Table.."
+        columns = ["Name","Score"]
+        sbquery = "CREATE TABLE " + "Scoreboard" + "(" + columns[0] + " VARCHAR(50)," + columns[1] + " INT NOT NULL" + ");"
+        #EX: https://github.com/jat023/CS304_DB/blob/master/src/ca/ubc/cs/cs304/steemproject/access/oraclejdbc/InitializeDatabase.java
+        try: self.cursor.execute(sbquery)
+        except MySQLdb.Error, e:
+                print "MySQL Error: " + str(e)
+        else:
+                print("Table Creation Success")
+
+    #insert score into scoreboard for game
+    def insertScore(self, target):
+        print "INSERTING INTO SCOREBOARD DATABASE"
+
+        for line in target:
+            #get name and score
+            name = Signal.parseSignal(line)[0] 
+            score = Signal.parseSignal(line)[1]
+            #get count of how many scores in table
+            sbquery = "SELECT count(*), min(Score) FROM Scoreboard"
+            self.cursor.execute(sbquery)
+            result = self.cursor.fetchone()
+            count, Score = result
+            print "Count: " + str(count)
+            if (10 <= count):
+                #if there are 10 or more scores, remove rank 10 
+                sbquery = "SELECT max(Score), Name FROM Scoreboard"
+                self.cursor.execute(sbquery)
+                result = self.cursor.fetchone()
+                Score, Name = result
+                print "Score: " + str(Score)
+                print "Name: " + str(Name)
+                sbquery = "DELETE FROM Scoreboard WHERE Score = %s" % (Score,)
+                print "Delete Rank 10"
+                self.cursor.execute(sbquery)
+                #Commit changes to the database
+                self.db.commit()
+                print "Finished deleting score"
+            #enter new score into table
+            sbquery = "INSERT INTO " + "Scoreboard" + " VALUES ( " + "'" + name + "'" + " ," + "'" + score + "'" + ");"
+            print("Inserting a row: " + sbquery)
+            self.cursor.execute(sbquery)
+            # Commit your changes in the database
+            self.db.commit()
+
+    #get scores from scoreboard table
+    def getTopScores(self, list):
+       print "GET TOP 10 SCORES"
+       #sort score by lowest to highest
+       sbquery = "SELECT Name, Score FROM Scoreboard ORDER BY Score ASC, Name ASC"
+       self.cursor.execute(sbquery)
+       result = self.cursor.fetchall()
+       #get amount of score 
+       length = len(result)
+       print "Number of scores: " + str(length)
+       for i in range(0, length):
+            name, score = result[i]
+            print str(i) + ", Name: " + str(name) + ", Time: " + str(score)
+
+       print "Finished getting scores"
