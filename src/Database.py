c3a869ecf2497e2f75e319c81c7672e9f8e9a86f
coolyoshi <yoonchris@live.ca>
2016-01-28 18:04:29 -0800

    changed tabs to spaces

--- src/Database.py
+++ src/Database.py
@@ -20,15 +20,15 @@ class Database:
 
     def isCurrentDate(self, date):
         return self.currentDate == date
-	
+    
     def setCurrentDate(self, date):
         self.currentDate = date
 
-	#generates a COUNT table with name = DATE_Count
-	#One table per day would be fastest. Should delete old tables since they arnt really needed
-	#(can use COUNT function on existing signla data to get count values)
-	def createCountTable(self, date):
-		print "Creating Count Table.."
+    #generates a COUNT table with name = DATE_Count
+    #One table per day would be fastest. Should delete old tables since they arnt really needed
+    #(can use COUNT function on existing signla data to get count values)
+    def createCountTable(self, date):
+        print "Creating Count Table.."
         columns = ["Black","Green","Blue","Grey"]
         query = "CREATE TABLE " + date + "_Count" + "(" + columns[0] + " INT," + columns[1] + " INT," + columns[2] + " INT," + columns[3] + " INT" + ");"
         try: self.cursor.execute(query)
@@ -36,13 +36,13 @@ class Database:
                 print "MySQL Error: " + str(e)
         else:
                 print("Table Creation Success")
-			
-	def insertCount(self, black, green, blue, grey):
-		query = "UPDATE " + date + "_count" + " SET Black =" + black + ",Green =" + green + ",Blue =" + blue + ",Grey = " + grey + ";" #WHERE some_column=some_value;
-		self.cursor.execute(query)
-		self.db.commit()
-	
-	#generates a BFG table with name based off of date
+            
+    def insertCount(self, black, green, blue, grey):
+        query = "UPDATE " + date + "_count" + " SET Black =" + black + ",Green =" + green + ",Blue =" + blue + ",Grey = " + grey + ";" #WHERE some_column=some_value;
+        self.cursor.execute(query)
+        self.db.commit()
+    
+    #generates a BFG table with name based off of date
     def createBFGTable(self, date):
         print "Creating BFG Table.."
         columns = ["ID","Location","Date","Time"]
@@ -53,9 +53,9 @@ class Database:
                 print "MySQL Error: " + str(e)
         else:
                 print("Table Creation Success")
-	
-	#one table per day??
-	#table names by date?
+    
+    #one table per day??
+    #table names by date?
     def insertBuffer(self, target):
         for line in target:
             color = Signal.parseSignal(line)[0]
@@ -71,29 +71,29 @@ class Database:
             self.cursor.execute(query)
             # Commit your changes in the database
             self.db.commit()
-			
-	def updateDatabase(self, target, black, green, blue, grey):
-		print "INSERTING INTO DATABASE"
+            
+    def updateDatabase(self, target, black, green, blue, grey):
+        print "INSERTING INTO DATABASE"
         date = time.strftime("%d_%m_%Y") #get current date
         if not self.isCurrentDate(date): #check if the date has changed
             self.createCountTable(date)
             self.createBFGTable(date)
             self.setCurrentDate(date)
-			
+            
         self.insertCount(black, green, blue, grey)
         self.insertBuffer(target)
-		
-	#Select: http://zetcode.com/db/mysqlpython/
-	#there should be only one row in the table
-	def pullCount(self):
-		query = "SELECT *" + " FROM " + self.currentDate + "_count;"
-		self.cursor.execute(query)
-		if( self.cursor.rowcount > 0 ):
-			rows = self.cursor.fetchall()
-			result = rows[0]
-			return result
-		else:
-			print "ERROR: No rows"
+        
+    #Select: http://zetcode.com/db/mysqlpython/
+    #there should be only one row in the table
+    def pullCount(self):
+        query = "SELECT *" + " FROM " + self.currentDate + "_count;"
+        self.cursor.execute(query)
+        if( self.cursor.rowcount > 0 ):
+            rows = self.cursor.fetchall()
+            result = rows[0]
+            return result
+        else:
+            print "ERROR: No rows"
 
 
     #database functions for the sorting game application
