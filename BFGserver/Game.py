56224d24f5049cb9e3f85d9ea59d192c6359d3f4
s-teph <stephial@hotmail.com>
2016-01-29 19:10:02 -0800

    Integrated database for scoreboard into game

--- BFGserver/Game.py
+++ BFGserver/Game.py
@@ -3,7 +3,8 @@
 
 import web
 import sys
-sys.path.insert(0,'/var/www/BinFunGame/src')
+#sys.path.insert(0,'/var/www/BinFunGame/src')
+sys.path.insert(0,'C:/Users/Steph/Desktop/Steph/UBC/Y4/T1/Capstone/BinFunGame/src')
 from Database import Database
 
 class Game:
@@ -11,28 +12,33 @@ class Game:
     app = None
     render = None
     scoreboardList = []
+    scoredb = None
 
     def __init__(self):
         urls = ('/', 'Game')
         self.render = web.template.render('templates/')
         self.app = web.application(urls, globals())
+        #create database for scores
+        self.scoredb = Database("54.218.32.132", "bfguser", "bfg123", "bfg")
+        self.scoredb.createScoreboardTable()
 
-    
     @staticmethod
     def startGame():
         BFGGame = Game()
         BFGGame.app.run()
-        #create database for scores
-        scoredb = Database("54.218.32.132", "bfguser", "bfg123", "bfg")
-        scoredb.createScoreboardTable()
 
     def getTopScoreboardList(self):
+        self.scoredb.getTopScores(self.scoreboardList)
         #print "get top 10"
-        #print self.scoreboardList[0:10]
+        print self.scoreboardList[0:10]
         return self.scoreboardList[0:10]
 
     def submitToScoreboard(self,entry):
-        self.scoreboardList.append(entry)
+        #add score to the database
+
+        self.scoredb.insertScore(entry)
+
+        #self.scoreboardList.append(entry)
         #print "This is scoreboardList"
         #print self.scoreboardList
         #self.scoreboardList.sort()
@@ -49,6 +55,8 @@ class Game:
         #Submit
         if(web.input().submit == "True"):
             entry = {'name':web.input().name,'score':web.input().score}
+            #print entry['score']
+            #print entry['name']
             self.submitToScoreboard(entry)
         return
 
