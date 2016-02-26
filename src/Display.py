c5595f4846e73bdd5464e26c96d3408dc6288b21
coolyoshi <yoonchris@live.ca>
2016-02-25 17:28:40 -0800

    aaaaa

--- src/Display.py
+++ src/Display.py
@@ -53,9 +53,9 @@ class Display:
     def getAllAccuracy(self):
         #colorAccuracy = [globalVars.greenCount,globalVars.greyCount,globalVars.blueCount,globalVars.blackCount]
         yesterday = datetime.now() - timedelta(days=1)
-        print "pulling from: " + yesterday.strftime("%Y-%d-%m")
+        print "pulling from: " + yesterday.strftime("%Y-%m-%d")
         displayDB = Database("54.218.32.132", "bfguser", "bfg123", "bfg")
-        colorAccuracy = displayDB.pullAccuracy(yesterday.strftime("%Y-%d-%m"))
+        colorAccuracy = displayDB.pullAccuracy(yesterday.strftime("%Y-%m-%d"))
         displayDB.turnOff()
         print colorAccuracy
         return colorAccuracy
