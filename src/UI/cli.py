dcfd6b8626fcd12506775c73b566da53eb448b0f
s-teph <stephial@hotmail.com>
2016-03-24 07:03:34 -0700

    client ui

--- src/UI/cli.py
+++ src/UI/cli.py
@@ -1,6 +1,6 @@
 import sys
-sys.path.insert(0,'/var/www/BinFunGame/src')
-#sys.path.insert(0,'C:/Users/Steph/Desktop/Steph/UBC/Y4/T1/Capstone/BinFunGame/src')
+#sys.path.insert(0,'/var/www/BinFunGame/src')
+sys.path.insert(0,'C:/Users/Steph/Desktop/Steph/UBC/Y4/T1/Capstone/BinFunGame/src')
 import web
 from Database import Database
 from CSVfile import CSVfile
@@ -15,8 +15,8 @@ from CSVfile import CSVfile
 bindata = []
 filePath = ""
 #location to store files
-relativePath = "/var/www/BinFunGame/src/UI/files/"
-#relativePath = "C:/Users/Steph/Desktop/Steph/UBC/Y4/T1/Capstone/BinFunGame/src/UI/files/"
+#relativePath = "/var/www/BinFunGame/src/UI/files/"
+relativePath = "C:/Users/Steph/Desktop/Steph/UBC/Y4/T1/Capstone/BinFunGame/src/UI/files/"
 urls = ('/', 'default',
         '/downloadfile', 'downloadfile')
 render = web.template.render('Website/')
@@ -26,24 +26,18 @@ db = Database("54.218.32.132", "bfguser", "bfg123", "bfg")
 
 #retrieve data from database, and return csv file to user
 def getData(entry):
-    print "get Data"
     #clear bindata list before getting new data
     del bindata[:]
     db.getBinData(bindata, entry)
     #create new CSV file to put data in 
     CSV = CSVfile(relativePath, entry['startdate'], entry['enddate'], entry['binlocation'], entry['color'])
-    #print returned bin data
+    #get # of entries so they can be added to CSV
     length = len(bindata)
     for i in range(0, length):
-        ID = bindata[i]['ID']
-        Location = bindata[i]['Location']
-        Date = bindata[i]['Date']
-        Time = bindata[i]['Time']
         #put data in CSV file
-        target = ID + "," + Location + "," + str(Date) + "," + str(Time)
+        target = bindata[i]['ID'] + "," + bindata[i]['Location'] + "," + str(bindata[i]['Date']) + "," + str(bindata[i]['Time'])
         CSV.quickAppend(target)
-        #print "Entry " + str(i) + ": " + str(ID) + ", " + str(Location) + ", " + str(Date) + ", " + str(Time)
-    print "done getting Data"
+    print "Done putting data into CSV"
 
 class default:
     def GET(self):
@@ -60,22 +54,21 @@ class default:
             #Start Date & End Date: YYYY-MM-DD
             entry = {'startdate':web.input().startdate, 'enddate':web.input().enddate, 'binlocation':web.input().binlocation, 'color':web.input().color}
             getData(entry)
+            #setting filePath to the path of the requested file
+            #so it can be sent to user to download
             global filePath
             filePath = entry['startdate'] + "_" + entry['enddate'] + "_" + entry['binlocation'] + "_" + entry['color'] + ".csv"
-            #raise web.seeother('/downloadfile')
-            #print filePath
         return
 
 class downloadfile:
     def GET(self):
+        #sends file to user to download
+        #opens up a download window
         global filePath
         f = relativePath + filePath
-        #print "downloadfile: " + filePath
-        #f = 'C:/Users/Steph/Desktop/Steph/UBC/Y4/T1/Capstone/BinFunGame/src/UI/files/2016-03-01_2016-03-20_nest_black.csv'
         getFile = file(f, 'rb')
         web.header('Content-Type','text/csv')
         web.header('Content-disposition', 'attachment; filename=' + filePath)
-        web.header('Content-transfer-encoding','base64')
         return getFile.read()
 
 
