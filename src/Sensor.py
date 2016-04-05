b200aee6a70dafeaf5e604c569f78861f7bb23ec
sebastian-lee <sebastian.lee.9@gmail.com>
2016-04-04 20:54:29 -0700

    Revert "removed useless code and added comments to all methods"

--- src/Sensor.py
+++ src/Sensor.py
@@ -7,8 +7,8 @@ from Signal import Signal
 class Sensor:
     __isEmpty = True    #flag used by Queue to see if the Sensor is active
     __signal = ''   #the current Signal string, see Signal class for format
-    __ID = ''   #the sensor ID, corresponds to the colour
-    __Location = '' #the location of the sensor/recycling station
+    __ID = ''   #the sensor ID
+    __Location = '' #the location of the sensor
         
     #GPIO Input Pin for the Sensor
     __gpiopin1 = 7
@@ -16,38 +16,26 @@ class Sensor:
     __timestamp = ''
 
     #set up GPIO
-    #constructor to set all the class flags accordingly
     def __init__(self, ID, Location):
         self.__ID = ID
         self.__Location = Location
-        self.setGPIOPin(ID)
-
-        #setup the GPIO pins
         GPIO.setmode(GPIO.BOARD)
         GPIO.setwarnings(False)
-        
-	    self.__timestamp = datetime.datetime.now()
+        #self.__gpiopin = self.getGPIOPin(ID)
+        self.setGPIOPin(ID)
+	self.__timestamp = datetime.datetime.now()
     
         GPIO.setup(self.__gpiopin1, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
         GPIO.setup(self.__gpiopin2, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-        GPIO.add_event_detect(self.__gpiopin1, GPIO.RISING, callback=self.sendSignal)
-        GPIO.add_event_detect(self.__gpiopin2, GPIO.RISING, callback=self.sendSignal)
+        GPIO.add_event_detect(self.__gpiopin1, GPIO.RISING, callback=self.testSendSignal)
+        GPIO.add_event_detect(self.__gpiopin2, GPIO.RISING, callback=self.testSendSignal)
 
-    #This is the callback method when a sensors detects that an object has passed through.
-    #Note that an old signal will be overwritten
-    def sendSignal(self, pinNumber):
-    #note that a timedelta is needed due to the use of two sensors
+    def testSendSignal(self, pinNumber):
 	a = self.__timestamp + datetime.timedelta(milliseconds=500)
 	if datetime.datetime.now() > a:
-	    #print "signal callback from signal: " + str(pinNumber)
+	    print "signal callback from signal: " + str(pinNumber)
+	    self.sendSignal()
 
-        #record the current time and create the string that will be stored into the database
-	    self.__timestamp = datetime.datetime.now()
-        self.__signal = Signal.initializeSignal(self.__ID, self.__Location, self.__timestamp)
-        #indicate that a signal was detected
-        self.setEmptyFlag(False)
-
-    #sets the sensor to the appropriate GPIO pin. The pins are set according to the colour of bin that the sensor is placed in. Each bin has two sensors and the pins have been optimized to make cable management easier.
     def setGPIOPin(self, ID):
         if ID is "black":
             self.__gpiopin1 = 16 
@@ -68,7 +56,27 @@ class Sensor:
         else:
             return 
 
-    #sets the empty flag to the given empty variable, also used by Queue class
+    #wait for sensor to turn on
+    def listenSignal(self):
+        #if(GPIO.input(self.__gpiopin)):
+        #    self.sendSignal()
+        #print "WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW"
+        #while True:
+        #GPIO.wait_for_edge(self.__gpiopin, GPIO.RISING)
+        #self.sendSignal()
+        return
+        
+    #refactor into signal class to parse and initialize signal
+    def sendSignal(self):           
+        #send date, time and id
+        #need to send to queue instead 
+	self.__timestamp = datetime.datetime.now()
+        dateTime = datetime.datetime.utcnow()
+        print dateTime
+        self.__signal = Signal.initializeSignal(self.__ID, self.__Location, dateTime)
+        self.setEmptyFlag(False)
+
+    #sets the empty flag to the given empty variable, used by Queue class
     def setEmptyFlag(self, empty):
         self.__isEmpty = empty
         
@@ -79,3 +87,30 @@ class Sensor:
     def getSignal(self):
         self.setEmptyFlag(True)
         return self.__signal
+	
+        
+    # now we'll define two threaded callback functions  
+    # these will run in another thread when our events are detected  
+    def my_callback(channel):
+        print "falling edge detected on 15"
+    def my_callback2(channel):
+        print "falling edge detected on 12"
+    def my_callback3(channel):
+        print "falling edge detected on 18"
+    def my_callback4(channel):
+        print "falling edge detected on 7" 
+    	
+    # when a falling edge is detected on port __gpiopin, regardless of whatever   
+    # else is happening in the program, the function my_callback will be run
+    #__init__(self, 1, "Nest")
+    #GPIO.add_event_detect(19, GPIO.FALLING, callback=my_callback, bouncetime=300)
+    #GPIO.add_event_detect(12, GPIO.FALLING, callback=my_callback, bouncetime=300)
+    #GPIO.add_event_detect(18, GPIO.FALLING, callback=my_callback, bouncetime=300)
+    #GPIO.add_event_detect(7, GPIO.FALLING, callback=my_callback, bouncetime=300)
+	
+    #Another example of interrupts:
+    #try:  
+    #   GPIO.wait_for_edge(23, GPIO.FALLING)  
+    #except KeyboardInterrupt:  
+    #   GPIO.cleanup()       # clean up GPIO on CTRL+C exit  
+    #GPIO.cleanup()           # clean up GPIO on normal exit 
